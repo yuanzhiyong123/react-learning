6fb53ae45db5a5e60c8cea433818a384390da560
yuanzhiyong <1060915552@qq.com>
2018-07-05 00:14:27 +0800

    'add'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { Button, List } from 'antd-mobile';
 export default class App extends React.Component {
   render() {
     const boss = '李云龙';
     return (
       <div>
         <div>独立团，团长：{boss}</div>
-        <Yiying boss='张大苗' /> 
+        <Yiying boss='张大苗' />
         <Qibinglian boss='孙德胜' />
       </div>
     )
@@ -19,25 +19,25 @@ function Qibinglian(props) {
 class Yiying extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      slider: ['小明','xiaohong' ,'嚣张']
+    this.state = {
+      slider: ['小明', 'xiaohong', '嚣张']
     }
   }
   addSlider() {
     this.setState({
-      slider:[...this.state.slider, '新兵蛋子'+Math.random()]
+      slider: [...this.state.slider, '新兵蛋子' + Math.random()]
     });
   }
   render() {
     return (
       <div>
-        <div>一营营长：{this.props.boss}</div> 
-        <button onClick={this.addSlider.bind(this)}>添加新兵</button>
-        <ul>
+        <div>一营营长：{this.props.boss}</div>
+        <Button type="primary" onClick={this.addSlider.bind(this)}>添加新兵</Button>
+        <List renderHeader={()=>'士兵列表'}>
           {this.state.slider.map((item, index) => {
-            return <li key={index}>{item}</li>
+            return <List.Item key={index}>{item}</List.Item>
           })}
-        </ul>
+        </List>
       </div>
     )
   }
