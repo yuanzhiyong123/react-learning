7e7957af2254bae3e945addd1d5b437309b83bd1
yuanzhiyong <1060915552@qq.com>
2018-07-16 21:48:33 +0800

    '完善消息列表页'

--- src/page/msg/msg.js
+++ src/page/msg/msg.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { List, WhiteSpace, WingBlank } from 'antd-mobile';
 
 @connect(state => state)
 export default class Msg extends React.Component {
-
+  getLast(arr) {
+    return arr[arr.length - 1];
+  }
   render() {
     console.log(this.props);
+    const Item = List.Item;
+    const Brief = Item.Brief;
+    const userId = this.props.user._id;
     let group = {};
     this.props.chat.msgList.forEach(v => {
       if (!group[v.chatid]) {
         group[v.chatid] = [];
-      }else{
+      } else {
         group[v.chatid].push(v);
       }
     });
     console.log(group);
+    const msgList = Object.values(group);
+    console.log(msgList);
     return (
-      <div>MSG</div>
+      <WingBlank>
+        {msgList.map(v => {
+          const target = userId === v[0].from ? v[0].to : v[0].from;
+          const name = this.props.chat.users[target].username ? this.props.chat.users[target].username : '';
+          const avatar = this.props.chat.users[target].avatar ? this.props.chat.users[target].avatar : '';
+          return (
+            <div key={v[0]._id}>
+              <WhiteSpace />
+              <List>
+                <Item
+                  thumb={require(`../../img/${avatar}`)}
+                >
+                  {this.getLast(v).content}
+                  <Brief>
+                    {name}
+                  </Brief>
+                </Item>
+              </List>
+            </div>
+          )
+        })}
+      </WingBlank>
     )
   }
 };
